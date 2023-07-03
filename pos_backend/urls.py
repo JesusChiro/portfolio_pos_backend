e128dd5ed104c0657c7e2bad9a19f80aaa60ed85
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-07-03 12:31:29 -0500

    deployment 2

--- pos_backend/urls.py
+++ pos_backend/urls.py
@@ -13,6 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from django.urls import re_path
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,32 +27,36 @@ from rest_framework_simplejwt.views import (
 '''
 Para Swagger
 '''
-from django.urls import re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="POS API",
-      default_version='v1',
-      description="API Rest para sistema punto de venta",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="jesus_chiroque@hotmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="POS API",
+        default_version='v1',
+        description="API Rest para sistema punto de venta",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="jesus_chiroque@hotmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
+
+def index(request):
+    return '<h1><center> API POS BACKEND VERSION 1.0.0</center></h1>'
+
+
 urlpatterns = [
+    path('', index),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/admin/', include('api_admin.urls')),
-    path('<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('docs<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 
 ]
